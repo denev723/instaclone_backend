e931436fa51e5aa6c9cca5c40d39f100be4be95b
denev <denev723@gmail.com>
2023-09-20 00:04:07 +0900

    4.23 ~ 4.26

---
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -0,0 +1,40 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    seeFollowers: async (_, { username, page }) => {
+      const checkUser = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      if (!checkUser) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
+      const followers = await client.user
+        .findUnique({
+          where: { username },
+        })
+        .followers({
+          take: 5,
+          skip: (page - 1) * 5,
+        });
+      const totalFollowers = await client.user.count({
+        where: {
+          following: {
+            some: {
+              username,
+            },
+          },
+        },
+      });
+      return {
+        ok: true,
+        followers,
+        totalPages: Math.ceil(totalFollowers / 5),
+      };
+    },
+  },
+};
